a05b627d8ea49e841ba3611754e9301fc2b98cd1
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-06 14:38:19 -0500

    Initail commit

---
+++ more_functions/validate_input_in_functions.py
@@ -0,0 +1,15 @@
+"""
+Morgan Christensen
+
+Module6: Validate_input)in_functions
+prompts user for name and score then prints values
+
+10/06/20
+"""
+
+def score_input(test_name, test_score = 0, invalid_message="Invalid test score try again"):
+    pass
+
+
+if __name__ == '__main__':
+    print(score_input("morgan", 65))
