5be5c8249678982c0eafb2ffff92f6f1f6c1ea39
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-06 14:59:47 -0500

    First test passes and function is created with doc string

--- more_functions/validate_input_in_functions.py
+++ more_functions/validate_input_in_functions.py
@@ -7,8 +7,21 @@ prompts user for name and score then prints values
 10/06/20
 """
 
+
 def score_input(test_name, test_score = 0, invalid_message="Invalid test score try again"):
-    pass
+    """
+    Function that accepts params and returns a string
+    :param test_name: the name of the test taker
+    :type test_name String
+    :param test_score: the score that they recieved
+    :type test_score int
+    :param invalid_message: Message displayed when an invalid score is provided
+    :type invalid_message: String
+    :return: formmated string with test_name, and test_score
+    :rtype String
+    """
+    #print("{}: {}".format(test_name, test_score))
+    return "{}: {}".format(test_name, test_score)
 
 
 if __name__ == '__main__':
