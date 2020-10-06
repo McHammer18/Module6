72355f795a5a67fb8df9d8699280ce0d927a9cf9
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-06 16:51:27 -0500

    Initial commit

---
+++ source_files/inner_functions_assignment.py
@@ -0,0 +1,18 @@
+"""
+Morgan Christensen
+
+Module6 - inner functions
+program accepts a list of measurements for a rectangle
+and calculates the perimeter and the area using a main and inner function
+
+10/06/20
+"""
+
+
+def measures(rect_measure):
+
+    return "Perimeter = {} Area = {}".format(p_value, a_value)
+
+
+if __name__ == '__main__':
+    print(measures([2.1, 3.4]))
