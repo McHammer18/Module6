65f728d2c12b8fee70da166293844330aa98ac14
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-06 17:10:48 -0500

    Unit test are created and pass and the function runs smoothly while gracefully handling exception Errors

--- source_files/inner_functions_assignment.py
+++ source_files/inner_functions_assignment.py
@@ -9,10 +9,35 @@ and calculates the perimeter and the area using a main and inner function
 """
 
 
-def measures(rect_measure):
+def measurements(rect_measure):
+    a_value = 0.0
+    p_value = 0.0
 
+    def area(a_measure):
+        if len(a_measure) == 1:
+            return a_measure[0] * a_measure[0]
+        elif len(a_measure) == 2:
+            return a_measure[0] * a_measure[1]
+        else:
+            raise IndexError
+
+    def perimeter(p_measure):
+        if len(p_measure)==1:
+            return p_measure[0] * 4
+        elif len(p_measure)==2:
+            return (p_measure[0] * 2) + (p_measure[1] * 2)
+        else:
+            raise IndexError
+    try:
+        a_value = area(rect_measure)
+        p_value = perimeter(rect_measure)
+    except IndexError:
+        raise IndexError
     return "Perimeter = {} Area = {}".format(p_value, a_value)
 
 
 if __name__ == '__main__':
-    print(measures([2.1, 3.4]))
+    try:
+        print(measurements([2, 3]))
+    except IndexError:
+        print("Invalid set of measurements!")
--- test_functions/test_inner_function.py
+++ test_functions/test_inner_function.py
@@ -1,10 +1,15 @@
 import unittest
-
+from source_files import inner_functions_assignment as inner
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_measurements_rectangle(self):
+        self.assertEqual(inner.measurements([2.1, 3.4]), "Perimeter = 11.0 Area = 7.14")
 
+    def test_measurements_square(self):
+        self.assertEqual(inner.measurements([3.5]), "Perimeter = 14.0 Area = 12.25")
 
+    def test_measurements_too_many_measurements(self):
+        with self.assertRaises(IndexError):
+            inner.measurements([7.4, 10.3, 4])
 if __name__ == '__main__':
     unittest.main()
